2cc4feb938eed09d428b93e1a70347172b6219e1
hajihong <hajihong96@gmail.com>
2022-08-08 15:47:23 +0900

    frisst

--- src/test/java/com/example/deploytest/DeploytestApplicationTests.java
+++ src/test/java/com/example/deploytest/DeploytestApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.deploytest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DeploytestApplicationTests {
 
 	@Test
